d71d52948fae1995b355bcc8d691df0d56fc8881
guhborda1 <guhborda@gmail.com>
2024-07-15 23:08:51 -0300

    feat: responsivedialog

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -20,62 +20,35 @@ const TodoList = ({ tasks }: TodoListProps) => {
 
     return (
         <>
-
-            <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead></TableHead>
-                        <TableHead className="w-[100px]">Tarefa</TableHead>
-                        <TableHead>Actions</TableHead>
-
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {tasks.map((task) => (
-                        <TableRow key={task.id}>
-                            <TableCell>{task.checked}</TableCell>
-                            <TableCell className="font-medium">{task.text}</TableCell>
-                            <TableCell className="font-medium"></TableCell>
-
-                        </TableRow>
-                    ))}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TableCell colSpan={3}>Total</TableCell>
-                        <TableCell className="text-right">$2,500.00</TableCell>
-                    </TableRow>
-                </TableFooter>
-            </Table>
-
             {
                 tasks.length > 0 ? (
-                    <div className="overflow-x-auto w-full justify-between">
-                        <table className="table w-full gap-2 ">
-                            {/* head */}
-                            <thead className="mb-6">
-                                <tr className="border-b border-solid border-gray-400">
-                                    <th className="flex justify-start">
-                                        <label>
-                                            <Checkbox />
-                                        </label>
-                                    </th>
-                                    <th className="ml-2 text-left">Tarefa</th>
-                                    <th className="flex justify-end">Ações</th>
-                                </tr>
-                            </thead>
-                            <tbody className="flex-col gap-3 mt-3">
-                                {tasks.map((task) => (
-                                    <Task key={task.id} task={task} />
-                                )).reverse()}
+                    <Table>
+                        <TableCaption>Lista de Tarefas</TableCaption>
+                        <TableHeader className="w-full">
+                            <TableRow className={`fex justify-around`}>
+                                <TableHead><Checkbox /></TableHead>
+                                <TableHead className="w-[100px]">Tarefa</TableHead>
+                                <TableHead>Actions</TableHead>
 
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody className="w-full">
+                            {tasks.map((task) => (
+                                <TableRow key={task.id}>
+                                    <TableCell>{task.checked}</TableCell>
+                                    <TableCell className="font-medium">{task.text}</TableCell>
+                                    <TableCell className="font-medium"></TableCell>
 
-                            </tbody>
-
-
-                        </table>
-                    </div>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TableCell colSpan={3}>Total</TableCell>
+                                <TableCell className="text-right">$2,500.00</TableCell>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
                 ) : (<div className="overflow-x-auto w-full justify-center flex">
                     <div className="h2 text-center">Adicione sua primeira tarefa!</div></div>)
             }
